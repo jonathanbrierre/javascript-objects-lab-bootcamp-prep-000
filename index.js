d8f3996b730680302180ec4c81fd56a7fc89e37b
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-03 20:25:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  var newObject = Object.assign({},object)
+  delete newObject[key]
+  return newObject
 }
 
 
